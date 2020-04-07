8d67b99ed2fb3b8caa75609cf7711609d013a824
Rob <alekseicruz@gmail.om>
2020-04-07 15:23:40 -0700

    Fixed lint issues

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+$(document).ready(() => {
   // Getting references to our form and input
-  var signUpForm = $('form.signup');
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+  const signUpForm = $('form.signup');
+  const emailInput = $('input#email-input');
+  const passwordInput = $('input#password-input');
 
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function(event) {
+  signUpForm.on('submit', (event) => {
     event.preventDefault();
-    var userData = {
+    const userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
     };
 
     if (!userData.email || !userData.password) {
@@ -17,26 +17,26 @@ $(document).ready(function() {
     }
     // If we have an email and password, run the signUpUser function
     signUpUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
+    emailInput.val('');
+    passwordInput.val('');
   });
+
+  function handleLoginErr(err) {
+    $('#alert .msg').text(err.responseJSON);
+    $('#alert').fadeIn(500);
+  }
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
   function signUpUser(email, password) {
-    $.post("/api/signup", {
-      email: email,
-      password: password
+    $.post('/api/signup', {
+      email,
+      password,
     })
-      .then(function(data) {
-        window.location.replace("/members");
+      .then((data) => {
+        window.location.replace('/members');
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
-  }
-
-  function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
   }
 });
