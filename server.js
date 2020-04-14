18f63f31f1825b764fa27ffa514690c2d3d4aae7
Aleksei Robert Cruz <alekseicruz@gmail.om>
2020-04-13 20:48:31 -0700

    public folder is working

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+// Requiring path to so we can use relative routes to our HTML files
+const path = require('path');
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, './public/members.html'));
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', socket => {
+  socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
 });
