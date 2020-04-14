5decd57e1070423c91d0f13ed03ebe56e8285985
Aleksei Robert Cruz <alekseicruz@gmail.om>
2020-04-13 17:13:59 -0700

    Changes to handlebars

--- routes/chat-routes.js
+++ routes/chat-routes.js
@@ -17,23 +17,3 @@
 //   console.log("listening on *:" + port);
 // });
 
-module.exports = (app) => {
-  // eslint-disable-next-line global-require
-    const http = require('http').Server(app);
-    const io = require('socket.io')(http);
-    const port = process.env.PORT || 3000;
-
-    app.get('/', (req, res) => {
-        res.sendFile(`${__dirname}/index.html`);
-    });
-
-    io.on('connection', (socket) => {
-        socket.on('chat message', (msg) => {
-            io.emit('chat message', msg);
-        });
-    });
-
-    http.listen(port, () => {
-        console.log(`listening on *:${port}`);
-    });
-};
