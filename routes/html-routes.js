c4b4c0ad6d35ce75fa33f9277020f32d63319241
Aleksei Robert Cruz <alekseicruz@gmail.om>
2020-04-14 17:39:46 -0700

    fixed eslint

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
 module.exports = (app) => {
-
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/members.html'));
     });
-}
-
+};
