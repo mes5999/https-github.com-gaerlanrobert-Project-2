7b925b5dc9d4878f011c7e0c2542dc0919c60b56
Aleksei Robert Cruz <alekseicruz@gmail.om>
2020-04-14 17:44:53 -0700

    fixed news json

--- routes/news.js
+++ routes/news.js
@@ -2,11 +2,14 @@ const fetch = require('node-fetch');
 
 module.exports = {
     async topic(topic) {
-        const today = new Date();
+        const now = new Date();
+        console.log(now);
         const apiKey = 'acef062b396a4219a8e009b9395a424d';
         const country = 'US';
-        const from = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
+        const from = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+        console.log(from);
         const url = `http://newsapi.org/v2/top-headlines?apiKey=${apiKey}&country=${country}&q=${topic}&sortBy=popularity&from=${from}`;
+        console.log(url);
         const response = await fetch(url);
         const { articles } = await response.json();
         console.log(articles);
