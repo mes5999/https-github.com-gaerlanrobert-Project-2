b6768ec3f60c3e7fa28c7b33f676f063b0e92a2d
Aleksei Robert Cruz <alekseicruz@gmail.om>
2020-04-15 18:28:42 -0700

    Got authentication to work

--- public/js/members.js
+++ public/js/members.js
@@ -2,9 +2,7 @@ $(document).ready(() => {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
     // eslint-disable-next-line no-extend-native
-    const capitalize = (str) => {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    };
+    const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
     $.get('/api/user_data').then((data) => {
         localStorage.setItem('username', capitalize(data.email.split('@').shift()));
