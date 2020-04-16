e0db25b256a46d1df329a0e13ad21b40337f03d2
Aleksei Robert Cruz <alekseicruz@gmail.om>
2020-04-15 18:25:23 -0700

    Got authentication to work

--- public/js/members.js
+++ public/js/members.js
@@ -1,8 +1,13 @@
 $(document).ready(() => {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
+    // eslint-disable-next-line no-extend-native
+    const capitalize = (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    };;
+
     $.get('/api/user_data').then((data) => {
-        localStorage.setItem('username', data.email.split('@').shift());
+        localStorage.setItem('username', capitalize(data.email.split('@').shift()));
         $('.member-name').text(localStorage.getItem('username'));
     });
 });
